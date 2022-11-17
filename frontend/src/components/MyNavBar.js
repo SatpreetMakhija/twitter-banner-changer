cfc7404056ccd9b8b270a07bf74f84616028a582
SatpreetMakhija <sm.satpreet@gmail.com>
2022-11-17 18:47:08 +0530

    logout works

--- frontend/src/components/MyNavBar.js
+++ frontend/src/components/MyNavBar.js
@@ -2,13 +2,30 @@ import React from "react";
 import {Container, Navbar, Button, Image} from 'react-bootstrap';
 import useStore from "../Store";
 import axios from "axios";
-
+import {Navigate} from "react-router-dom";
 
 function MyNavBar () {
 
+    // const navigate = useNavigate();
+
     const user = useStore(state => state.user);
     const userLoginStatus = useStore(state => state.user);
+    const resetUserLoginStatus = useStore(state => state.resetUserLoginStatus);
+    const resetUser = useStore(state => state.resetUser);
 
+    const  logOutUser = async () => {
+      const response = await axios.get("http://localhost:8000/logout", {withCredentials: true})
+      if (response.data.message === "userLoggedOut") {
+        console.log("user is logged out..")
+        resetUserLoginStatus();
+        resetUser();
+      } else {
+        //Show a sandwitch in frontend saying the logout did not work. Try again. 
+        console.log("Some error occured while logging out..")
+      }
+  
+    
+    }
     
 
     return (
@@ -24,7 +41,7 @@ function MyNavBar () {
             </Navbar.Text>
             {userLoginStatus ? <Navbar.Text>
               <Image src={user.profileImageUrl} roundedCircle/>
-              {/* <Button onClick={logOutUser}>Logout</Button> */}
+              <Button onClick={logOutUser}>Logout</Button>
             </Navbar.Text> : ""}
             
           </Navbar.Collapse>
