2917cbeb0b35f9422e986eb385b506fd91771cb0
SatpreetMakhija <sm.satpreet@gmail.com>
2022-12-04 19:26:19 +0530

    we can now see the user albums in frontend dynamically

--- frontend/src/pages/ShowAlbum.js
+++ frontend/src/pages/ShowAlbum.js
@@ -16,27 +16,27 @@ function ShowAlbum() {
     useEffect(  () => {
         async function fetchData() {
             try {
-                let albumData = await axios.get("http://localhost:8000/album/" + album_id, {withCredentials: true});
-            console.log(albumData);
-            return albumData;
+                let response = await axios.get("http://localhost:8000/album/" + album_id, {withCredentials: true});
+            console.log("The album data is ")
+                console.log(response.data.album);
+            return response.data.album;
             }
             catch (err) {
                 console.log(err);
             }
         }
-        let albumData = fetchData();
-        setalbumData(albumData);
+       fetchData().then((data) => setalbumData(data));
     }, [])
+
+    const Albums = () => {
+        return <Stack gap={3} style = {{margin: "5vw", alignContent:"center"}}>
+            {albumData.bannersURLs.map((url) => <img src={'http://localhost:8000/' + url} style={{width: "80vw"}}/>) } 
+        </Stack>
+    }
 
     return (
         <div>
-            {albumData ? "Here's your album data.." : 'loading....'}
-        <Stack gap={3} style = {{margin: "5vw", alignContent:"center"}}>
-        <img src={exampleBanner} style={{width: "80vw"}}/>
-        <img src={exampleBanner} style={{width: "80vw"}}/>
-        <img src={exampleBanner} style={{width: "80vw"}}/>
-        </Stack>
-
+            {albumData ? <Albums/> : 'loading....'}
         </div>
     )
 }
