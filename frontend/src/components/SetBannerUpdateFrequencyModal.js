2c1ccb9640c2cf6f1f175d761e49bd4b22181f57
SatpreetMakhija <sm.satpreet@gmail.com>
2022-12-05 22:05:02 +0530

    clean: used prettier to make code pretty

--- frontend/src/components/SetBannerUpdateFrequencyModal.js
+++ frontend/src/components/SetBannerUpdateFrequencyModal.js
@@ -2,14 +2,10 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 function SetBannerUpdateFrequencyModal(props) {
-
-
-
   const url_parameters = useParams();
   const album_id = url_parameters.albumid;
-  console.log(album_id);
 
   const [show, setShow] = useState(false);
   const [bannerUpdateFrequency, setBannerUpdateFrequency] = useState(null);
@@ -18,24 +14,23 @@ function SetBannerUpdateFrequencyModal(props) {
 
   const handleValueChange = (e) => {
     setBannerUpdateFrequency(e.target.value);
-  }
+  };
 
   const setAlbumFrequency = () => {
-    
     //make axios call here to set the album.
-    //to do so get album id from url. 
-    //call api set-album 
-    let response = {status: '200'};
-    //use a sandwich to give this update to the user. 
-    if (response.status === '200') {
+    //to do so get album id from url.
+    //call api set-album
+    let response = { status: "200" };
+    //use a sandwich to give this update to the user.
+    if (response.status === "200") {
       //show sandwich with success notification
       handleClose();
       props.setShowToast();
     } else {
-      //show sandwich with error notification. 
-      console.log("An error occured")
+      //show sandwich with error notification.
+      console.log("An error occured");
     }
-  }
+  };
   return (
     <>
       <Button variant="primary" onClick={handleShow}>
@@ -52,21 +47,25 @@ function SetBannerUpdateFrequencyModal(props) {
           <Modal.Title>Choose the update frequency for the album</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <Form.Select aria-label="Default select example" onChange={handleValueChange}>
-      <option>Open this select menu</option>
-      <option value="1">1 hour</option>
-      <option value="3">3 hour</option>
-      <option value="6">6 hour</option>
-      <option value="12">12 hour</option>
-      <option value="24">24 hour</option>
-      
-    </Form.Select>
+          <Form.Select
+            aria-label="Default select example"
+            onChange={handleValueChange}
+          >
+            <option>Open this select menu</option>
+            <option value="1">1 hour</option>
+            <option value="3">3 hour</option>
+            <option value="6">6 hour</option>
+            <option value="12">12 hour</option>
+            <option value="24">24 hour</option>
+          </Form.Select>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={setAlbumFrequency}>Set Album</Button>
+          <Button variant="primary" onClick={setAlbumFrequency}>
+            Set Album
+          </Button>
         </Modal.Footer>
       </Modal>
     </>
