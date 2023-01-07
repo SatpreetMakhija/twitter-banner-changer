04209d36de92bb7b41b8f8b025c1acdb55ed1b08
SatpreetMakhija <sm.satpreet@gmail.com>
2023-01-07 12:52:22 +0530

    [ui]: added basic footer

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Row, Col, Navbar } from 'react-bootstrap';
+
+function Footer() {
+  return (
+    <Navbar bg="light" >
+      <Container>
+        <Row>
+          <Col>
+            <Navbar.Text>
+              Copyright &copy; {new Date().getFullYear()}
+            </Navbar.Text>
+          </Col>
+          {/* <Col className="text-right">
+            <Navbar.Text>
+              
+            </Navbar.Text>
+          </Col> */}
+        </Row>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Footer;
