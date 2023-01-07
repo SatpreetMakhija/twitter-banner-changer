38d883a60693ecd48c029c1320a885c9889e3b42
SatpreetMakhija <sm.satpreet@gmail.com>
2023-01-07 12:35:19 +0530

    [notification time in browser's local time

--- frontend/src/components/Notification.js
+++ frontend/src/components/Notification.js
@@ -2,12 +2,24 @@ import React from "react";
 import Stack from 'react-bootstrap/Stack';
 import Card from "react-bootstrap/Card";
 function Notification(props) {
+    
+
+    function convertGMTToLocal(gmtDate) {
+        const options = {
+          hour: 'numeric',
+          minute: 'numeric'
+        };
+        const localDate = new Intl.DateTimeFormat('en-US', options).format(gmtDate);
+        return localDate;
+      }
+
+
   return (
     <Stack gap={3}>
     {props.jobsArray.map((jobData) => {
         if (jobData.failReason) {
           return (<Card bg="danger">
-          <Card.Body>Banner change failed at {jobData.failedAt}</Card.Body>
+          <Card.Body>Banner change failed at {convertGMTToLocal(new Date(jobData.failedAt))}</Card.Body>
         </Card>)
         } else {
             return (<Card bg="success">
