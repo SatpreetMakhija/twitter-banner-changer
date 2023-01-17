371fce23a070dbd07c0dd2c2807e8e7bd5691ab4
SatpreetMakhija <sm.satpreet@gmail.com>
2023-01-17 20:23:21 +0530

    [error fix]: added key attribute to notifications map function

--- frontend/src/components/Notification.js
+++ frontend/src/components/Notification.js
@@ -16,13 +16,13 @@ function Notification(props) {
 
   return (
     <Stack gap={3}>
-    {props.jobsArray.map((jobData) => {
+    {props.jobsArray.map((jobData, index) => {
         if (jobData.failReason) {
-          return (<Card bg="danger">
+          return (<Card bg="danger" key={index}>
           <Card.Body>Banner change failed at {convertGMTToLocal(new Date(jobData.failedAt))}</Card.Body>
         </Card>)
         } else {
-            return (<Card bg="success">
+            return (<Card bg="success" key={index}>
             <Card.Body>Banner changed at {convertGMTToLocal(new Date(jobData.lastFinishedAt))}</Card.Body>
           </Card>)
         }
